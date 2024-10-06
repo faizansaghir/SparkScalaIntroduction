8d2e3d2772359625b762150085128d00d03e6d64
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-06 21:25:48 +0530

    Added spark code to get max temperature per station

--- src/main/scala/com/sundogsoftware/spark/MaxTemperatures.scala
+++ src/main/scala/com/sundogsoftware/spark/MaxTemperatures.scala
@@ -1,10 +1,12 @@
 package com.sundogsoftware.spark
 
-import org.apache.spark._
+import breeze.linalg.max
 import org.apache.log4j._
-import scala.math.max
+import org.apache.spark._
 
-/** Find the maximum temperature by weather station for a year */
+import scala.math.min
+
+/** Find the minimum temperature by weather station */
 object MaxTemperatures {
   
   def parseLine(line:String): (String, String, Float) = {
@@ -23,18 +25,29 @@ object MaxTemperatures {
     // Create a SparkContext using every core of the local machine
     val sc = new SparkContext("local[*]", "MaxTemperatures")
     
+    // Read each line of input data
     val lines = sc.textFile("data/1800.csv")
+    
+    // Convert to (stationID, entryType, temperature) tuples
     val parsedLines = lines.map(parseLine)
+    
+    // Filter out all but TMAX entries
     val maxTemps = parsedLines.filter(x => x._2 == "TMAX")
+    
+    // Convert to (stationID, temperature)
     val stationTemps = maxTemps.map(x => (x._1, x._3.toFloat))
+    
+    // Reduce by stationID retaining the minimum temperature found
     val maxTempsByStation = stationTemps.reduceByKey( (x,y) => max(x,y))
+    
+    // Collect, format, and print the results
     val results = maxTempsByStation.collect()
     
     for (result <- results.sorted) {
        val station = result._1
        val temp = result._2
        val formattedTemp = f"$temp%.2f F"
-       println(s"$station max temperature: $formattedTemp") 
+       println(s"$station maximum temperature: $formattedTemp")
     }
       
   }
