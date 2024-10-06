fab35c05bab91973cd72856a2f25ea1661a27a79
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-06 22:05:57 +0530

    Added spark code to get amount spent by customer from customer-orders.csv file

---
+++ src/main/scala/com/sundogsoftware/spark/TotalSpentByCustomers.scala
@@ -0,0 +1,20 @@
+package com.sundogsoftware.spark
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+object TotalSpentByCustomers {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "TotalSpentByCustomers")
+    val lines = sc.textFile("data/customer-orders.csv")
+    val customerData = lines.map(parseCustomerData)
+    val spendPerCustomer = customerData.reduceByKey((x, y) => x+y)
+    spendPerCustomer.foreach(println)
+  }
+
+  private def parseCustomerData(line: String): (Int, Float) = {
+    val parts = line.split(",")
+    (parts(0).toInt, parts(2).toFloat)
+  }
+}
